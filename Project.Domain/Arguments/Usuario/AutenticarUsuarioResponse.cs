fc6c8009aaff15286120e8209ab7ea6553ae931d
Eduardo <user_mrttgvtef311@tribanco.com.br>
2023-01-31 17:21:22 -0300

    Definindo contratos do domínio com interfaces

---
+++ Project.Domain/Arguments/Usuario/AutenticarUsuarioResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Domain.Arguments.Usuario
+{
+    public class AutenticarUsuarioResponse
+    {
+        public Guid Id { get; set; }
+        public string PrimeiroNome { get; set; }
+    }
+}
---
+++ Project.Domain/Interface/Services/IServiceUsuario.cs
@@ -0,0 +1,17 @@
+﻿using Project.Domain.Arguments.Usuario;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Domain.Interface.Services
+{
+    public interface IServiceUsuario
+    {
+        AdicionarUsuarioResponse AdicionarUsuario(AdicionarUsuarioRequest request);
+
+        AutenticarUsuarioResponse AutenticarUsuario(AutenticarUsuarioRequest request);
+    }
+
+}
